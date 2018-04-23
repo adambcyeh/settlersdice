bfa7f4aba7fb1b6670a07e1cb9cd4ede15bd289b
Adam Yeh <adamyeh@gmail.com>
2018-04-23 00:01:40 -0700

    resetbarbs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 const MAX_BARB = 6;
 function getRandomInt(max) {
@@ -7,44 +7,44 @@ function getRandomInt(max) {
 }
 
 function speak(txt) {
-  window.speechSynthesis.speak(new SpeechSynthesisUtterance(txt))
+  window.speechSynthesis.speak(new SpeechSynthesisUtterance(txt));
 }
 const eventroll = () => {
   const roll = getRandomInt(6);
-  if (roll ===  1) {
-    return 'yellow';
+  if (roll === 1) {
+    return "yellow";
   } else if (roll === 2) {
-    return 'green';
+    return "green";
   } else if (roll === 3) {
-    return 'blue';
+    return "blue";
   }
-  return 'barbarian';
-}
+  return "barbarian";
+};
 
 class App extends Component {
   state = {
     dice1: 1,
     dice2: 1,
-    event: 'barbarian',
-    barbarianCount: 0,
-  }
-  newBarbCount = (event) => {
-    if (event !== 'barbarian') {
+    event: "barbarian",
+    barbarianCount: 0
+  };
+  newBarbCount = event => {
+    if (event !== "barbarian") {
       return this.state.barbarianCount;
     }
-    if (this.state.barbarianCount == MAX_BARB ) {
-      speak('barbarians attack!')
+    if (this.state.barbarianCount == MAX_BARB) {
+      speak("barbarians attack!");
       return 0;
     }
     return this.state.barbarianCount + 1;
-  }
+  };
   onClick = () => {
     const newd1 = getRandomInt(6);
     const newd2 = getRandomInt(6);
     const event = eventroll();
 
     speak(String(newd1 + newd2));
-    if (event === 'barbarian') {
+    if (event === "barbarian") {
       speak(event);
     } else {
       speak(event + " " + newd2);
@@ -57,24 +57,34 @@ class App extends Component {
       event: event,
       barbarianCount: newCount
     });
-
-  }
-
+  };
+  resetBarbarians = () => {
+    this.setState({ barbarianCount: 0 });
+    speak("resetting barbarians");
+  };
   render() {
     return (
       <div className="App">
-        <div className="wrapper" onClick={this.onClick}>
-          <div className="dice1">
-            <span>{this.state.dice1}</span>
-          </div>
-          <div className="dice2">
-            <span>{this.state.dice2}</span>
-          </div>
-          <div className="dicebarb">
-            <span>{this.state.event}</span>
+        <div className="clicktarget" onClick={this.onClick}>
+          <div className="wrapper">
+            <div className="dice1">
+              <span>{this.state.dice1}</span>
+            </div>
+            <div className="dice2">
+              <span>{this.state.dice2}</span>
+            </div>
+            <div className="dicebarb">
+              <span>{this.state.event}</span>
+            </div>
           </div>
         </div>
-        <span>Barbarian progress: {this.state.barbarianCount} </span>
+        <span className="barbarianstate">
+          Barbarians: {this.state.barbarianCount} / {MAX_BARB + 1}{" "}
+        </span>
+        <div className="resetbarb" onClick={this.resetBarbarians}>
+          {" "}
+          Reset Barbarians{" "}
+        </div>
       </div>
     );
   }
